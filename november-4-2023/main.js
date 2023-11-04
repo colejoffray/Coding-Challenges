71ef584a4c871dbf870c80a3380b9bada4df8967
Cole Joffray <142552258+colejoffray@users.noreply.github.com>
2023-11-04 12:22:03 -0400

    code war for november 4

---
+++ november-4-2023/main.js
@@ -0,0 +1,36 @@
+//KATA 1 
+
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+function removeSmallest(numbers) {
+  let indexOfLowest = numbers.indexOf(Math.min(...numbers))
+  let newNums = []
+  numbers.forEach((num, i) => {
+      if(i != indexOfLowest){
+          newNums.push(num)
+      }
+  })
+return newNums
+}
+
+//test cases 
+console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
+console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]");
+console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]");
+
+
+////////////////////////////////////////////////////////////////////////////
+
