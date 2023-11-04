e50853d416ca3ec62e8f199fd8c6567a212caf22
Cole Joffray <142552258+colejoffray@users.noreply.github.com>
2023-11-04 12:18:34 -0400

    challenge completed

--- november-3-2023/main.js
+++ november-3-2023/main.js
@@ -30,3 +30,40 @@ console.log(binaryArrayToNumber([1,1,1,1]), 15)
 
 
 ////////////////////////////////////////////////////////////////////////////
+
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// Example 2:
+// a1 = ["tarp", "mice", "bull"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns []
+
+// Notes:
+// Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+// In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+// Beware: In some languages r must be without duplicates.
+
+function inArray(array1,array2){
+  //...
+  let results = []
+  array2.forEach(word => {
+    array1.forEach(ending => {
+      if(word.includes(ending)){
+        results.push(ending)
+
+      }
+})
+  })
+    return Array.from(new Set(results)).sort()
+  }
+
+console.log(inArray(["arp", "live", "strong"],  ["lively", "alive", "harp", "sharp", "armstrong"]), ["arp", "live", "strong"])
