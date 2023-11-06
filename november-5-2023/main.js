213047b3ada15da919c5433b1f2ea354823d3e6e
Cole Joffray <142552258+colejoffray@users.noreply.github.com>
2023-11-05 22:07:03 -0500

    6kyu

--- november-5-2023/main.js
+++ november-5-2023/main.js
@@ -37,3 +37,39 @@ console.log(solution('samurai', 'ai'), true)
 console.log(solution('abcde', 'abc'), false)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//KATA 3 6kyu
+
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+function likes(names) {
+    // TODO
+    const length = names.length
+    if(!length){
+      return 'no one likes this'
+    }else if (length === 1){
+      return `${names[0]} likes this`
+    }else if (length === 2){
+      return `${names[0]} and ${names[1]} like this`
+    }else if (length === 3){
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    }else if (length > 3)  {
+      return `${names[0]}, ${names[1]} and ${names.slice(2).length} others like this`
+    }
+  }
+
+//test cases 
+console.log(likes([]), 'no one likes this');
+console.log(likes(['Peter']), 'Peter likes this');
+console.log(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+console.log(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
