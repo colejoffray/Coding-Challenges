5e2743280169a6f66ca777daf3f14bcbb455f3e7
Cole Joffray <142552258+colejoffray@users.noreply.github.com>
2023-11-05 21:52:44 -0500

    7kyu

--- november-5-2023/main.js
+++ november-5-2023/main.js
@@ -16,3 +16,24 @@ const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest'
 
 console.log(greet('Daniel', 'Daniel'), 'Hello boss');
 console.log(greet('Greg', 'Daniel'), 'Hello guest');
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//KATA 2 7kyu
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+// STRINGSFUNDAMENTALS
+
+const solution = (str, ending) => str.endsWith(ending);
+
+//test cases
+console.log(solution('samurai', 'ai'), true)
+console.log(solution('abcde', 'abc'), false)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
