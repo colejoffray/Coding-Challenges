e962c9e053bce6be1bdfabd6c3d8a2cde18e17b8
Cole Joffray <colej@MacBook-Pro.lan>
2023-11-07 11:56:14 -0800

    7kyu kata

--- november-7-2023/main.js
+++ november-7-2023/main.js
@@ -1,6 +1,6 @@
 //ARRAY LADDER 
 
-//KATA 1
+//KATA 1 8kyu
 
 // Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
@@ -15,6 +15,44 @@ function sumMix(x){
 console.log(sumMix([9, 3, '7', '3']), 22);
 console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
 console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
+
+////////////////////////////////////////////////////////////////////////////
+
+//KATA 2 7kyu
+
+// Introduction and Warm-up (Highly recommended)
+// Playing With Lists/Arrays Series
+// Task
+// Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+
+// Notes
+// Array/list will contain positives only .
+// Array/list will always have even size
+// Input >> Output Examples
+// minSum({5,4,2,3}) ==> return (22) 
+// Explanation:
+// The minimum sum obtained from summing each two integers product ,  5*2 + 3*4 = 22
+// minSum({12,6,10,26,3,24}) ==> return (342)
+// Explanation:
+// The minimum sum obtained from summing each two integers product ,  26*3 + 24*6 + 12*10 = 342
+// minSum({9,2,8,7,5,4,0,6}) ==> return (74)
+// Explanation:
+// The minimum sum obtained from summing each two integers product ,  9*0 + 8*2 +7*4 +6*5 = 74
+
+function minSum(arr) {
+  // your code here
+  return arr.sort((a,b) => a-b).reduce((acc,c) => acc + c * arr.pop(), 0)
+
+}
+
+//test cases
+console.log(minSum([5,4,2,3]), 22);
+console.log(minSum([12,6,10,26,3,24]), 342);
+console.log(minSum([9,2,8,7,5,4,0,6]), 74);
+
+
+
+
 
 
   
